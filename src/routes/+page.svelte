4a693dc79f933b5686d1b78a8aff021f4e1ec3bd
jinhyo-dev <jinhyo0850@gmail.com>
2023-01-07 01:58:09 +0900

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let count: number = 0
+
+	const increaseCount = () => {
+		count += 1
+	}
+</script>
+
+<h1>count : {count}</h1>
+<button on:auxclick={increaseCount}>Click</button>
