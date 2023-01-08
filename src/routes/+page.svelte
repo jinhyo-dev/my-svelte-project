79692db7352aeeecc21a1581fb39ddd4d6d3e2fd
jinhyo-dev <jinhyo0850@gmail.com>
2023-01-08 23:58:06 +0900

    main logo added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,32 @@
-<script lang="ts">
-	let count: number = 0
-
-	const increaseCount = () => {
-		count += 1
-	}
+<script lang='ts'>
+	const logo = new URL('../images/logo.png', import.meta.url).href;
 </script>
 
-<h1>count : {count}</h1>
-<button on:auxclick={increaseCount}>Click</button>
+<div class='logo-container' id='logo-container'>
+	<img src={logo} alt='useLaboratory' loading='eager'>
+</div>
+
+<style lang='scss'>
+    .logo-container {
+				width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+				& img {
+					width: 50%;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          animation: changeSize 3s ease forwards;
+				}
+    }
+
+    @keyframes changeSize {
+        0% {
+						margin-top: 30vh;
+        }
+        100% {
+            width: 25%;
+						margin-top: 3vh;
+        }
+    }
+</style>
